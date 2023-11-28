6d899f8e6e0edcf3b5ed4aed56acb1a078efaf0d
Shivansh Verma <115868006+shivansh-verma13@users.noreply.github.com>
2023-11-29 01:25:07 +0530

    Flask Backend

--- app.py
+++ app.py
@@ -1,79 +1,76 @@
-import streamlit as st
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+from app.recommendation_logic import (
+    recommend,
+    fetch_poster,
+    fetch_credits,
+    fetch_movie_details,
+)
 import pickle
 import pandas as pd
-import difflib
-import requests
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+# Load movies data
+movies_dict = pickle.load(open("movies2_dict.pkl", "rb"))
+movies = pd.DataFrame(movies_dict)
+
+# Load similarity data
+similarity = pickle.load(open("similarity2.pkl", "rb"))
 
 
-
-def fetch_poster(movie_id):
-  response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=ce365e0bb0a3897036362ade305c4a5c'.format(movie_id))
-  data = response.json()
-  return "https://image.tmdb.org/t/p/w500/" +  data['poster_path']
+@app.route("/")
+def index():
+    return render_template("index.html", movies=movies_dict)
 
 
-def recommend(movie):
-
-  list_of_all_titles = movies['title'].tolist()
-
-  find_close_match = difflib.get_close_matches(movie, list_of_all_titles)
-
-  close_match = find_close_match[0]
-  index_of_the_movie = movies[movies.title == close_match]['index'].values[0]
-  similarity_score = list(enumerate(similarity[index_of_the_movie]))
-  sorted_similar_movies = sorted(similarity_score, key = lambda x:x[1], reverse = True)
-
-  recommended_movies = []
-  recommended_movies_posters = []
-
-  i = 1
-  for movie in sorted_similar_movies[1:]:
-    index = movie[0]
-    title_from_index = movies[movies.index==index]['title'].values[0]
-    id_from_index = movies[movies.index==index]['id'].values[0]
-
-    if (i<6):
-      recommended_movies.append(title_from_index)
-      recommended_movies_posters.append(fetch_poster(id_from_index))
-      i+=1
-  return recommended_movies , recommended_movies_posters
+@app.route("/movie_names", methods=["GET"])
+def get_movie_names():
+    movie_names = movies_dict["title"]
+    return jsonify(movie_names)
 
 
-movies_dict = pickle.load(open('movies2_dict.pkl' , 'rb'))
-movies =pd.DataFrame(movies_dict)
-
-similarity = pickle.load(open('similarity2.pkl' , 'rb'))
-
-st.title('Movie Recommendation System')
-
-selected_movie_name = st.selectbox(
-    '' , movies['title'].values
-)
-
-if st.button('Recommend'):
-    Names,Posters = recommend(selected_movie_name)
-    
-    col1, col2, col3, col4, col5 = st.columns(5)
-
-    with col1:
-      st.text(Names[0])
-      st.image(Posters[0])
-
-    with col2:
-      st.text(Names[1])
-      st.image(Posters[1])
-
-    with col3:
-      st.text(Names[2])
-      st.image(Posters[2])
-
-    with col4:
-      st.text(Names[3])
-      st.image(Posters[3])
-
-    with col5:
-      st.text(Names[4])
-      st.image(Posters[4])
+@app.route("/get_credits/<int:movie_id>", methods=["GET"])
+def get_credits(movie_id):
+    cast, crew = fetch_credits(movie_id)
+    return jsonify({"cast": cast, "crew": crew})
 
 
+@app.route("/get_movie_details/<int:movie_id>", methods=["GET"])
+def get_movie_details(movie_id):
+    (
+        genres,
+        overview,
+        tagline,
+        vote_average,
+        release_date,
+        runtime,
+        adult,
+        backdrop_path,
+    ) = fetch_movie_details(movie_id)
+    return jsonify(
+        {
+            "genres": genres,
+            "overview": overview,
+            "tagline": tagline,
+            "vote_average": vote_average,
+            "release_date": release_date,
+            "runtime": runtime,
+            "adult": adult,
+            "backdrop_path": backdrop_path,
+        }
+    )
 
+
+@app.route("/recommend", methods=["POST"])
+def get_recommendations():
+    selected_movie_name = request.json.get("selected_movie")
+    names, posters, ids = recommend(selected_movie_name, movies, similarity)
+    # Convert int64 values to regular Python integers
+    id_values = [int(i) for i in ids]
+    return jsonify({"names": names, "posters": posters, "id": id_values})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
